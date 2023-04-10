a32eb39f7a259c7cabdb75346b5c70246deab9fe
Tippy <Pixie2shot@gmail.com>
2023-04-09 20:11:43 -0500

    routine activities api passing

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,18 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { UnauthorizedDeleteError, UnauthorizedUpdateError } = require('../errors.js');
+const { getRoutineActivityById, getRoutineById, destroyRoutineActivity, updateRoutineActivity, getUserById } = require('../db');
+const {JWT_SECRET = "neverTell"} = process.env;
 
 // PATCH /api/routine_activities/:routineActivityId
-router.patch('/', async(req, res, next) => {
-    try {
-     const { updateRoutineActivity } = req.params;
-     const fields = req.body;
-     const updatedRoutineActivityData = { updateRoutineActivity, fields }
-     const routineUpdate = await updateRoutineActivity(updatedRoutineActivityData);
-     res.send(routineUpdate)
+router.patch('/:routineActivityId', async(req, res, next) => {
+    try{
+     const authHeader = req.headers['authorization'];
+     const token = authHeader.split(' ')[1];
+     const userId = jwt.verify(token, JWT_SECRET)['id'];
+     const user = await getUserById(userId);
+     console.log("USER", user);
+     const id = req.params['routineActivityId'];
+     const routineActivity = await getRoutineActivityById(id)
+     const routine = await getRoutineById(routineActivity.routineId);
+     if(routine.creatorId !== userId) {
+        res.status(403).send({
+          "error": 'UnathorizedUpdateError',
+          "message":`${UnauthorizedUpdateError(user.username, routine.name)}`,
+          "name": `UnauthorizedUpdateError`
+        })
+      } else {
+        const fields = req.body;
+        const routineActUpdate = await updateRoutineActivity({id, ...fields});
+        res.send(routineActUpdate) ;
+      }
     }catch(error){
      next(error);
     }
-})
+  })
 // DELETE /api/routine_activities/:routineActivityId
-
+router.delete('/:routineActivityId', async(req, res, next) => {
+    try {
+     const authHeader = req.headers['authorization']
+     const token = authHeader.split(' ')[1];
+     const userId = jwt.verify(token, JWT_SECRET)["id"];
+     const user =  await getUserById(userId);
+     const id = req.params['routineActivityId'];
+     const routineActivity = await getRoutineActivityById(id);
+     const routine = await getRoutineById(routineActivity.routineId);
+     if(userId !== routine.creatorId) {
+       res.status(403).send({
+         "error": 'UnathorizedDeleteError',
+         "message": `${UnauthorizedDeleteError(user.username, routine.name)}`,
+         "name": `UnauthorizedDeleteError`
+       })
+     } else {
+       await destroyRoutineActivity(id);
+       res.send(routineActivity);
+     }
+    }catch(error){
+     next(error);
+    }
+  })
 module.exports = router;
